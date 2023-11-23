84a0e73c3c456ff1ef7e54a1b6e2905e2e5bfef2
SantiagoCorredor <69865633+SantiagoCorredor@users.noreply.github.com>
2023-11-23 10:13:48 -0500

    Revert "Actualización auto_gestion de reportes"
    This reverts commit 66e065beefd613bdea640bd08a496078d2cebfae.

--- auto_gestion_beta/applications/reports/urls.py
+++ auto_gestion_beta/applications/reports/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 from django.views.generic import TemplateView
-from .views import consultar_datos
 
 
 urlpatterns = [
-    path('consultas/', consultar_datos, name='consultar_datos'),
-]       
+    path('consulta/<str:tabla>/', views.consultar_datos, name='consultar_datos'),
+    # Otras rutas de tu aplicación
+    path('base/', TemplateView.as_view(template_name='base.html'), name='base_view'),
+]
